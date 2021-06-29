768af403edc05491fd6b3651d8d2f65cffa72f6d
tejabalu <tejabalu15@gmail.com>
2021-06-29 13:52:43 +0530

    final-1

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,53 +1,54 @@
-var picPaths = ['','/images/icons-01.svg','/images/icons-02.svg','/images/icons-03.svg'];
+var picPaths = [
+    "",
+    "/images/icons-01.svg",
+    "/images/icons-02.svg",
+    "/images/icons-03.svg",
+];
 var imgO = new Array();
 var curPic = 0;
 let flag = true;
-for(i=1; i <= picPaths.length; i++) {
+for (i = 1; i <= picPaths.length; i++) {
     imgO[i] = new Image();
     imgO[i].src = picPaths[i];
 }
 
-const timer = ms => new Promise(res => setTimeout(res, ms))
-
 function swapImage() {
     if (!flag) {
-        let tout = setTimeout(resumescroll, 3000);
         return;
     }
-    curPic = (++curPic > picPaths.length-1)? 1 : curPic;
+    curPic = ++curPic > picPaths.length - 1 ? 1 : curPic;
     imgCont.src = imgO[curPic].src;
-    setTimeout(swapImage,500);
+    setTimeout(swapImage, 500);
 }
 
-imgCont = document.getElementById('yournemesisid');
+imgCont = document.getElementById("yournemesisid");
 swapImage();
 
-
-function gameresult (playerchoice, computerchoice) {
+function gameresult(playerchoice, computerchoice) {
     switch (playerchoice) {
-        case 1:    
+        case 1:
             if (computerchoice == 1) {
-                draw();  
+                draw();
             } else if (computerchoice == 2) {
-                loose();    
+                loose();
             } else {
-                win();    
+                win();
             }
             break;
         case 2:
             if (computerchoice == 2) {
-                draw();  
+                draw();
             } else if (computerchoice == 3) {
-                loose();    
+                loose();
             } else {
                 win();
             }
             break;
         case 3:
             if (computerchoice == 3) {
-                draw();  
+                draw();
             } else if (computerchoice == 1) {
-                loose();    
+                loose();
             } else {
                 win();
             }
@@ -56,21 +57,21 @@ function gameresult (playerchoice, computerchoice) {
 }
 
 function playerchoice1() {
-    computerchoice = Math.round(Math.random()*(2.5)+0.5);
+    computerchoice = Math.round(Math.random() * 2.5 + 0.5);
     computerchoiceimg(computerchoice);
-    return gameresult(1, computerchoice)
+    return gameresult(1, computerchoice);
 }
 
 function playerchoice2() {
-    computerchoice = Math.round(Math.random()*(2.5)+0.5);
+    computerchoice = Math.round(Math.random() * 2.5 + 0.5);
     computerchoiceimg(computerchoice);
-    return gameresult(2, computerchoice)
+    return gameresult(2, computerchoice);
 }
 
 function playerchoice3() {
-    computerchoice = Math.round(Math.random()*(2.5)+0.5);
+    computerchoice = Math.round(Math.random() * 2.5 + 0.5);
     computerchoiceimg(computerchoice);
-    return gameresult(3, computerchoice)
+    return gameresult(3, computerchoice);
 }
 
 function computerchoiceimg(computerchoice) {
@@ -80,34 +81,63 @@ function computerchoiceimg(computerchoice) {
 
 let wincount = 0;
 let losscount = 0;
-document.getElementById("wins").textContent = `Wins: ${wincount}`
-document.getElementById("losses").textContent = `Losses: ${losscount}`
+
+document.getElementById("wins").textContent = `Wins: ${wincount}`;
+document.getElementById("losses").textContent = `Losses: ${losscount}`;
 
 function win() {
-    document.getElementById("wins").textContent = `Wins: ${wincount}`
-    document.getElementById("result").textContent = "You won!"
     wincount++;
+    document.getElementById("wins").textContent = `Wins: ${wincount}`;
+    document.getElementById("result").textContent = "You won!";
 }
 
 function loose() {
-    document.getElementById("losses").textContent = `Losses: ${losscount}`
-    document.getElementById("result").textContent = "You lost!"
     losscount++;
+    document.getElementById("losses").textContent = `Losses: ${losscount}`;
+    document.getElementById("result").textContent = "You lost!";
 }
 
 function draw() {
-    document.getElementById("result").textContent = "Draw!"
+    document.getElementById("result").textContent = "Draw!";
 }
-
-
 
 async function resumescroll() {
     flag = true;
     swapImage();
 }
 
-// window.setTimeout("closeresult()", 500);
+let idleTimer = null;
+let idleState = false;
+let swaptimeout = null;
+let rangevalue = 0;
 
-// function closeresult(){
-//     document.getElementById("result").style.display = "none";
-// }
+function showresult(time) {
+    flag = false;
+    clearTimeout(idleTimer);
+    if (idleState == true) {
+        $("#result").removeClass("inactive");
+    }
+    idleState = false;
+    idleTimer = setTimeout(function () {
+        $("#result").addClass("inactive");
+        idleState = true;
+    }, time);
+    clearTimeout(swaptimeout);
+    swaptimeout = setTimeout(() => {
+        flag = true;
+        swapImage();
+    }, 3000);
+    rangevalue = losscount - wincount;
+    console.log(rangevalue);
+    document.getElementById("range").value = rangevalue;
+}
+
+$(".uimages").click(function () {
+    showresult(2000);
+    showrange();
+});
+
+function showrange(wincount, losscount) {
+    rangevalue = losscount - wincount;
+    document.getElementsByClassName("slider").value = "rangevalue";
+}
